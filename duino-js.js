5dd15609bc4e69f44873815696ab872a8daf7716
jaden203 <124502873+jaden203@users.noreply.github.com>
2026-05-03 11:44:35 +0900

    Update duino-js.js

--- duino-js.js
+++ duino-js.js
@@ -17,7 +17,13 @@ function startMiner(username = `Hoiboy19`, rigid = `Duino-JS`, threads = 1, mini
     // Loop through the amount of threads
     for (let workerVer = 0; workerVer < threads; workerVer++) {
         // Create the worker
-        worker = new Worker(`worker.js`);
+        let worker = new Worker(`worker.js`); // let을 붙여서 독립적으로 생성
+
+        // ★ 추가된 부분: 워커에서 오는 메시지를 받아서 메인 콘솔에 찍어줌
+        worker.onmessage = function(event) {
+            console.log(event.data); 
+        };
+
         // Send the username, rigid and workerVer to the worker
         worker.postMessage([username, rigid, workerVer, miningkey]);
     }
