dc2ad91b334e38991fb083c48e6a4d04151fb65e
jaden203 <124502873+jaden203@users.noreply.github.com>
2026-05-03 11:51:57 +0900

    Update duino-js.js

--- duino-js.js
+++ duino-js.js
@@ -1,30 +1,12 @@
-// Get the amount of threads available
 const userThreads = navigator.hardwareConcurrency;
 
-// Define function with default username, rigid, amount of threads and mining key
 function startMiner(username = `Hoiboy19`, rigid = `Duino-JS`, threads = 1, miningkey = null) {
-    // Validate the amount of threads
-    if (threads < 1) {
-        threads = 1;
-    }
-    if (threads > 8) {
-        threads = 8;
-    }
-    if (threads > userThreads) {
-        threads = userThreads;
-    }
+    if (threads < 1) threads = 1;
+    if (threads > 8) threads = 8;
+    if (threads > userThreads) threads = userThreads;
 
-    // Loop through the amount of threads
     for (let workerVer = 0; workerVer < threads; workerVer++) {
-        // Create the worker
-        let worker = new Worker(`worker.js`); // let을 붙여서 독립적으로 생성
-
-        // ★ 추가된 부분: 워커에서 오는 메시지를 받아서 메인 콘솔에 찍어줌
-        worker.onmessage = function(event) {
-            console.log(event.data); 
-        };
-
-        // Send the username, rigid and workerVer to the worker
+        const worker = new Worker(`worker.js`);
         worker.postMessage([username, rigid, workerVer, miningkey]);
     }
 }
